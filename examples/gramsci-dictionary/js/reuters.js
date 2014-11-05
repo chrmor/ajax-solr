554ee6d1f0c5443a072ac4afc166ad8c578ecc96
Michele Nucci <mik.nucci@gmail.com>
2014-11-05 10:49:13 +0100

    Added Gramsci Dictionary

---
+++ examples/gramsci-dictionary/js/reuters.js
@@ -0,0 +1,82 @@
+var Manager;
+
+(function ($) {
+
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://metasound.dibet.univpm.it:8080/solr-gramsci-dictionary/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      id: 'result',
+      target: '#docs'
+    }));
+    Manager.addWidget(new AjaxSolr.PagerWidget({
+      id: 'pager',
+      target: '#pager',
+      prevLabel: '&lt;',
+      nextLabel: '&gt;',
+      innerWindow: 1,
+      renderHeader: function (perPage, offset, total) {
+        $('#pager-header').html($('<span style="margin:3pt"></span>').text('Stai visualizzando da ' + Math.min(total, offset + 1) + ' a ' + Math.min(total, offset + perPage) + ' di ' + total + ' risultati'));
+      }
+    }));
+    var fields = ['topic_ss', 'type_ss', 'norm_length_s', 'cites_quaderno_ss', 'author_s'];
+    for (var i = 0, l = fields.length; i < l; i++) {
+      Manager.addWidget(new AjaxSolr.FacetsWidget({
+        id: fields[i],
+        target: '#' + fields[i],
+        field: fields[i]
+      }));
+    }
+    Manager.addWidget(new AjaxSolr.CurrentSearchWidget({
+      id: 'currentsearch',
+      target: '#selection'
+    }));
+//    'Gramsci dictionary', 'DBpedia entity', 'Public Notebook'
+    Manager.addWidget(new AjaxSolr.AutocompleteWidget({
+      id: 'dic_title_text',
+      target: '#dic_title_search',
+      fields: [ 'label_s']
+    }));
+    Manager.addWidget(new AjaxSolr.AutocompleteWidget({
+      id: 'dic_text',
+      target: '#dic_search',
+      fields: [ 'text']
+    }));
+    Manager.init();
+    var query;
+    if (location.href.indexOf('?title=') != -1) {
+        query = 'label_s:"' + decodeURI(location.href.split('?title=')[1]) + '"';
+        // XXX: we need to remove the query string from the URL 
+        // to avoid bad behaviours with click on subsequent links in the pages....
+    } else {
+        query = "*:*";
+    }
+    Manager.store.addByValue('q', query);
+    var params = {
+      facet: true,
+      'facet.field': [ 'topic_ss', 'type_ss', 'norm_length_s', 'cites_quaderno_ss', 'label_s', 'author_s'],
+      'facet.limit': 200,
+      'facet.mincount': 1,
+      'json.nl': 'map',
+      'sort': 'label_s asc',
+      'rows': 50
+      
+    };
+    for (var name in params) {
+      Manager.store.addByValue(name, params[name]);
+    }
+
+    Manager.doRequest();
+  });
+
+  $.fn.showIf = function (condition) {
+    if (condition) {
+      return this.show();
+    }
+    else {
+      return this.hide();
+    }
+  }
+
+})(jQuery);
---
+++ examples/gramsci-dictionary/widgets/AutocompleteWidget.js
@@ -0,0 +1,96 @@
+(function ($) {
+
+AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractTextWidget.extend({
+
+  facetsNamesMapping : {"type_ss":"Tipo di voce", "norm_length_s":"Lunghezza voce", "topic_ss":"Tema", "cites_quaderno_ss":"Contiene citazioni da", "label_s":"Titolo della voce", "text":"Testo della voce"},
+
+  afterRequest: function () {
+    $(this.target).find('input').unbind().removeData('events').val('');
+
+    var self = this;
+
+    // sort autocomplete values b
+    var sortValues = function(a, b) {
+      var aVal = parseInt(a.counter);
+      var bVal = parseInt(b.counter);                  
+      var diff = bVal - aVal;
+      if (diff == 0) {
+        if (a.label < b.label)
+          return -1;
+        else if (a.label > b.label)
+          return 1;
+        else
+          return 0;
+      } else {
+        return diff;
+      }      
+    };
+
+    var callback = function (response) {
+      var list = [];
+      for (var i = 0; i < self.fields.length; i++) {
+        var field = self.fields[i];
+        for (var facet in response.facet_counts.facet_fields[field]) {
+          label = self.facetsNamesMapping[field];    
+          if (label == undefined) {
+              label = field;
+          } else {
+              label = label;
+          }
+          list.push({
+            field: field,
+            value: facet,
+            counter: response.facet_counts.facet_fields[field][facet],
+            label: facet + ' (' + response.facet_counts.facet_fields[field][facet] + ') - ' + label
+          });
+        }
+      }
+
+      self.requestSent = false;
+      $(self.target).find('input').autocomplete('destroy').autocomplete({
+        delay: 500,
+        source: function(request, response) {
+          var re = $.ui.autocomplete.escapeRegex(request.term);
+          var strtWithPattern = new RegExp("^" + re, "i");
+          
+          var a  = $.grep(list, function(item, index) {
+            return strtWithPattern.test(item.value);
+          });
+          a.sort(sortValues);
+          response(a);
+        },
+        select: function(event, ui) {
+          if (ui.item) {
+            self.requestSent = true;
+            if (self.manager.store.addByValue('fq', ui.item.field + ':' + AjaxSolr.Parameter.escapeValue(ui.item.value))) {
+              self.doRequest();
+            }
+          }
+        }
+      });
+
+      // This has lower priority so that requestSent is set.
+      $(self.target).find('input').bind('keydown', function(e) {
+        if (self.requestSent === false && e.which == 13) {
+          var value = $(this).val();
+          if (value && self.set(value)) {
+            self.doRequest();
+          }
+        }
+      });
+    } // end callback
+
+    var params = [ 'rows=0&facet=true&facet.limit=-1&facet.mincount=1&json.nl=map' ];
+    for (var i = 0; i < this.fields.length; i++) {
+      params.push('facet.field=' + this.fields[i]);
+    }
+    var values = this.manager.store.values('fq');
+    for (var i = 0; i < values.length; i++) {
+      params.push('fq=' + encodeURIComponent(values[i]));
+    }
+    params.push('q=' + this.manager.store.get('q').val());
+    $.getJSON(this.manager.solrUrl + 'select?' + params.join('&') + '&wt=json&json.wrf=?', {}, callback);
+  }
+});
+
+})(jQuery);
