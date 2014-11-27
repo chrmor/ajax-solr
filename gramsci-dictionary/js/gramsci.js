ff6dd53cf224f9efe600d2baa5a507708a68e356
Michele Nucci <mik.nucci@gmail.com>
2014-11-27 11:55:59 +0100

    Index updated/refactored

---
+++ gramsci-dictionary/js/gramsci.js
@@ -0,0 +1,91 @@
+var Manager;
+
+(function ($) {
+
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://gramsciproject.org:8080/solr-gramsci-dictionary/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      id: 'result',
+      target: '#docs'
+    }));
+    Manager.addWidget(new AjaxSolr.PagerWidget({
+      id: 'pager',
+      target: '#pager',
+      prevLabel: '&lt;',
+      nextLabel: '&gt;',
+      innerWindow: 1,
+      renderHeader: function (perPage, offset, total) {
+        $('#pager-header').html($('<span style="margin:3pt"></span>').text('Stai visualizzando da ' + Math.min(total, offset + 1) + ' a ' + Math.min(total, offset + perPage) + ' di ' + total + ' risultati'));
+      }
+    }));
+
+    var fields = ['topic_ss', 'type_ss', 'norm_length_s', 'cites_quaderno_ss', 'author_s'];
+    var facetsNamesMapping = {"type_ss":"Tipo di voce", "norm_length_s":"Lunghezza voce", "topic_ss":"Tema", "cites_quaderno_ss":"Contiene citazioni da", "label_s":"Titolo della voce", "text":"Testo della voce"};
+    
+    for (var i = 0, l = fields.length; i < l; i++) {
+      Manager.addWidget(new AjaxSolr.FacetsWidget({
+        id: fields[i],
+        target: '#' + fields[i],
+        field: fields[i]
+      }));
+    }
+    Manager.addWidget(new AjaxSolr.CurrentSearchWidget({
+      id: 'currentsearch',
+      target: '#selection',
+      facetsNamesMapping: facetsNamesMapping
+    }));
+    Manager.addWidget(new AjaxSolr.AutocompleteWidget({
+      id: 'dic_title_text',
+      target: '#dic_title_search',
+      fields: [ 'label_s'],
+      facetsNamesMapping: facetsNamesMapping,
+      submitOnlyIfTermSelect: true
+    }));
+    Manager.addWidget(new AjaxSolr.AutocompleteWidget({
+      id: 'dic_text',
+      target: '#dic_search',
+      fields: [ 'text'],
+      facetsNamesMapping: facetsNamesMapping,
+      submitOnlyIfTermSelect: false,
+      autocompleteOnlyOnStartWith: true
+    }));
+    Manager.init();
+    var query;
+    if (location.href.indexOf('?title=') != -1) {
+        query = 'label_s:"' + decodeURI(location.href.split('?title=')[1]) + '"';
+        // XXX: we need to remove the query string from the URL 
+        // to avoid bad behaviours with click on subsequent links in the pages....
+    } else {
+        query = "*:*";
+    }
+    Manager.store.addByValue('q', query);
+    Manager.store.addByValue('q.op', 'AND');
+    var params = {
+      facet: true,
+      'facet.field': [ 'topic_ss', 'type_ss', 'norm_length_s', 'cites_quaderno_ss', 'label_s', 'author_s', 'text'],
+      'facet.limit': 200,
+      'facet.mincount': 1,
+      'json.nl': 'map',
+      'sort': 'label_s asc',
+      'rows': 50
+      
+    };
+    for (var name in params) {
+      Manager.store.addByValue(name, params[name]);
+    }
+
+    Manager.doRequest();
+  });
+
+  $.fn.showIf = function (condition) {
+    if (condition) {
+      return this.show();
+    }
+    else {
+      return this.hide();
+    }
+  }
+
+})(jQuery);
---
+++ gramsci-dictionary/widgets/ResultWidget.js
@@ -0,0 +1,158 @@
+(function ($) {
+
+AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
+  start: 0,
+
+  beforeRequest: function () {
+    $(this.target).html($('<img>').attr('src', 'gramsci-dictionary/images/ajax-loader.gif'));
+  },
+
+  facetLinks: function (facet_field, facet_values) {
+    var links = [];
+    if (facet_values) {
+      for (var i = 0, l = facet_values.length; i < l; i++) {
+        if (facet_values[i] !== undefined) {
+          links.push(
+            $('<a href="#"></a>')
+            .text(facet_values[i])
+            .click(this.facetHandler(facet_field, facet_values[i]))
+          );
+        }
+        else {
+          links.push('no items found in current selection');
+        }
+      }
+    }
+    return links;
+  },
+
+  facetHandler: function (facet_field, facet_value) {
+    var self = this;
+    return function () {
+      self.manager.store.remove('fq');
+      self.manager.store.addByValue('fq', facet_field + ':' + AjaxSolr.Parameter.escapeValue(facet_value));
+      self.doRequest();
+      return false;
+    };
+  },
+
+  afterRequest: function () {
+    $(this.target).empty();
+    $(this.target).append('<div class="panel-group" id="accordion"></div>');
+    var accordion = $('#accordion');
+    for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
+      var doc = this.manager.response.response.docs[i];
+
+      if (this.manager.response.response.docs.length == 1)
+        accordion.append(this.template(doc, true));
+      else
+        accordion.append(this.template(doc, false));
+
+      var items = [];
+      items = items.concat(this.facetLinks('topics', doc.topics));
+      items = items.concat(this.facetLinks('organisations', doc.organisations));
+      items = items.concat(this.facetLinks('exchanges', doc.exchanges));
+
+      var $links = $('#links_' + doc.id);
+      $links.empty();
+      for (var j = 0, m = items.length; j < m; j++) {
+        $links.append($('<li></li>').append(items[j]));
+      }
+    }
+  },
+
+  template: function (doc, forceOpenAccordion) {
+    var snippet = '';
+
+    /*
+    var start;
+    var lastSpaceBar
+    if (doc.text[0].length > 600) {
+        start = doc.text[0].substring(0, 600);
+        lastSpaceBar = start.lastIndexOf(" ");
+        start = start.substring(0,lastSpaceBar);
+        snippet += ' ' + start + '...';
+    } else {
+        snippet += doc.text[0];
+    }
+    */
+
+    var output = "";
+    output += '<div class="panel panel-default">' +
+                        '<div class="panel-heading">' +
+                              '<h4>' +
+                                '<a data-toggle="collapse" data-parent="#accordion" href="#collapse' + doc.id + '">';
+
+    if (doc.label_s != null) {
+        output += doc.label_s;
+    } else {
+        output += 'Unnamed element';
+    }
+
+    output += '</a></h4s></div>';
+
+    var openPanels;
+    if (forceOpenAccordion /* location.href.indexOf('?uri=') != -1 */) {
+        openPanels = 'in';
+    } else {
+        openPanel = '';
+    }
+
+    output += '<div id="collapse' + doc.id + '" class="panel-collapse collapse ' + openPanels + '">' +
+                      '<div class="panel-body">';
+
+    if (doc.type_ss != undefined) {
+        output += '<p><strong>Type: ' + doc.type_ss + '</strong><p/>';
+    }
+
+
+    //snippet += doc.text[0];
+
+    var entryFile;
+    var localName = doc.uri_ss[0].split("/")[doc.uri_ss[0].split("/").length - 1];
+    entryFile = "gramsci-dictionary/dictionary_entries/" + localName + "/index.html";
+
+    $.ajax({
+      url: entryFile,
+      async: false
+    }).done(function(data) {
+      snippet += data;
+    });
+
+    //snippet += '<div class="small text-right"><a href="' + doc.uri_ss[0] + '" target="_blank">Vedi voce intera</a></div>';
+
+    output += '<p>' + snippet + '</p>';
+
+    /*
+    if (doc.annotated_by_ss != undefined && doc.notebook_id_ss != undefined) {
+        output += '<hr/>' + doc.annotated_by_ss + ' annotated this note. <a href="http://ask.thepund.it/#/notebooks/' + doc.notebook_id_ss[0] +'" target="_blank"><br/>See notebook at Ask.ThePund.it</a></div>';
+    }
+    */
+
+    output += '</div></div>';
+
+    output += '</div>';
+
+    return output;
+  },
+
+  init: function () {
+    $(document).on('click', 'a.more', function () {
+      var $this = $(this),
+          span = $this.parent().find('span');
+
+      if (span.is(':visible')) {
+        span.hide();
+        $this.text('more');
+      }
+      else {
+        span.show();
+        $this.text('less');
+      }
+
+      return false;
+    });
+  }
+});
+
+})(jQuery);
