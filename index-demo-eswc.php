c512776a52fb1774b494b6f60aed07940c5eb27c
Christian Morbidoni <christian.morbidoni@gmail.com>
2015-05-20 19:13:51 +0200

    different strategy for parsing fields...

--- index-demo-eswc.php
+++ index-demo-eswc.php
@@ -25,8 +25,8 @@ $pippo = file_get_contents('http://localhost:8080/solr-demo-eswc/collection1/sel
 //echo $pippo;
 $p = split('"facet_fields":',$pippo);
 $p = split('"tagType_ss":',$p[1]);
-$p = split(']',split('\[',$p[1])[1]);
-echo $p[0] . "<br/>";
+$p = split('[',$p[1]);
+echo $p[1] . "<br/>";
 //echo split(']',split('\[',split('"tagType_ss":',split('"facet_fields":',$pippo)[1])[1])[1])[0];
 //$rowList = split(']',split('\[',split('"tagType_ss":',split('"facet_fields":',$pippo)[1])[1])[1])[0];
 
